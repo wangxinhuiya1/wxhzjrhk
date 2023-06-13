9f921e8086525b07fe8f903ec2c69bd9d9c39a06
13459033546 <1370895785@qq.com>
2023-06-13 10:02:43 +0800

    init

--- src/http/paper.js
+++ src/http/paper.js
@@ -52,7 +52,7 @@ import axios from 'axios'
 
 // 创建自定义的 Axios 实例
 const api = axios.create({
-  baseURL: 'http://api.jqrjq.cn', // 设置基础URL
+  baseURL: 'https://api.jqrjq.cn', // 设置基础URL
   withCredentials: true, // 允许携带跨域请求的凭证
 })
 
